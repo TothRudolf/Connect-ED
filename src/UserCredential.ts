9694ebece6615cbb7b02b9dc46ea9fb1eb5fdc7a
szabi22 <szabolcskiss898@gmail.com>
2021-04-15 21:00:31 +0300

    oh crap

--- src/UserCredential.ts
+++
@@ -1,6 +0,0 @@
-type UserCredential = {
-    additionalUserInfo?: firebase.auth.AdditionalUserInfo | null;
-    credential: firebase.auth.AuthCredential | null;
-    operationType?: string | null;
-    user: firebase.User | null;
-  };
