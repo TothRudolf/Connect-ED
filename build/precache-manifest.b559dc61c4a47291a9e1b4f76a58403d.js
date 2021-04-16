2b6894db828a51e724514190451a841dfbb94c2c
Huba Peter <huba.ptr@gmail.com>
2021-04-16 22:04:35 +0300

    fire setup

---
+++ build/precache-manifest.b559dc61c4a47291a9e1b4f76a58403d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b1245fdae4ca8b62bacfafd0bd1da5a0",
+    "url": "./static/media/transparent.b1245fda.svg"
+  },
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "0542377bc84506cc7d05",
+    "url": "./static/js/main.0542377b.chunk.js"
+  },
+  {
+    "revision": "e434bc668339d1fe31a9",
+    "url": "./static/js/1.e434bc66.chunk.js"
+  },
+  {
+    "revision": "0542377bc84506cc7d05",
+    "url": "./static/css/main.b08c71fd.chunk.css"
+  },
+  {
+    "revision": "e434bc668339d1fe31a9",
+    "url": "./static/css/1.2bcb792d.chunk.css"
+  },
+  {
+    "revision": "90eb1d339e80cb88882be6d725348c14",
+    "url": "./index.html"
+  }
+];
